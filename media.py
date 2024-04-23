ec498fe54739305c5a7c3df0d193d5a867608b8f
PinoDePo <167258975+PinoDePo@users.noreply.github.com>
2024-04-22 21:53:19 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,8 @@
+# exercicio para resolver media de notas 
+
+nota_1 = float(input("DIGITE A NOTA 1"))
+nota_2 = float(input("DIGITE A NOTA 2"))
+
+media = (nota_1 + nota_2) / 2
+
+print(f" A NOTA FINAL É: {media} ")
