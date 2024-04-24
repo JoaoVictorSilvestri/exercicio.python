cf19435d9f87e372c47338cb5f3ea6520a959512
PinoDePo <167258975+PinoDePo@users.noreply.github.com>
2024-04-23 21:16:25 -0300

    Update media.py

--- media.py
+++ media.py
@@ -6,3 +6,12 @@ nota_2 = float(input("DIGITE A NOTA 2"))
 media = (nota_1 + nota_2) / 2
 
 print(f" A NOTA FINAL É: {media} ")
+
+
+
+#VERIFICAR SE A NOTA DO ALUNO E APROVADA OU REPROVADO COM A MEDIA MAIOR QUE 7
+
+if media >= 7:
+    print("VOCE FOI APROVADO")
+else:
+    print("VOCE FOI REPROVADO")
